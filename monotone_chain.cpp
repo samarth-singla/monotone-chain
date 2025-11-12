08e2e5de726be42b3fa3c9c47616e517b208cde0
samarth-singla <samysingla146@gmail.com>
2025-11-12 22:28:11 +0530

    hello

---
+++ monotone_chain.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+struct pt {
+    double x, y;
+};
+
+int orientation(pt a, pt b, pt c) {
+    double v = a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y);
+    if (v < 0) return -1; // clockwise
+    if (v > 0) return +1; // counter-clockwise
+    return 0;
+}
+
+bool cw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o < 0 || (include_collinear && o == 0);
+}
+bool ccw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o > 0 || (include_collinear && o == 0);
+}
+
+void convex_hull(vector<pt>& a, bool include_collinear = false) {
+    if (a.size() == 1)
+        return;
+
+    sort(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.x, a.y) < make_pair(b.x, b.y);
+    });
+    pt p1 = a[0], p2 = a.back();
+    vector<pt> up, down;
+    up.push_back(p1);
+    down.push_back(p1);
+    for (int i = 1; i < (int)a.size(); i++) {
+        if (i == a.size() - 1 || cw(p1, a[i], p2, include_collinear)) {
+            while (up.size() >= 2 && !cw(up[up.size()-2], up[up.size()-1], a[i], include_collinear))
+                up.pop_back();
+            up.push_back(a[i]);
+        }
+        if (i == a.size() - 1 || ccw(p1, a[i], p2, include_collinear)) {
+            while (down.size() >= 2 && !ccw(down[down.size()-2], down[down.size()-1], a[i], include_collinear))
+                down.pop_back();
+            down.push_back(a[i]);
+        }
+    }
+
+    if (include_collinear && up.size() == a.size()) {
+        reverse(a.begin(), a.end());
+        return;
+    }
+    a.clear();
+    for (int i = 0; i < (int)up.size(); i++)
+        a.push_back(up[i]);
+    for (int i = down.size() - 2; i > 0; i--)
+        a.push_back(down[i]);
+}
+void solve(ifstream &file,ofstream &outfile,int p)
+{
+    int n;
+    file>>n;
+    vector<pt>a(n);
+    for(int i=0;i<n;i++)
+    {
+        int x,y; file>>x>>y;
+        a[i].x=x;
+        a[i].y=y;
+    }
+    convex_hull(a,0);
+    outfile<<"CASE "<<p<<endl;
+    for(int i=0;i<a.size();i++)
+    {
+        outfile<<a[i].x<<" "<<a[i].y<<endl;
+    }
+}
+int main()
+{
+    ifstream file("");
+    ofstream outfile("output.txt");
+    int t;
+    file>>t;  
+    int p=1;  
+    while(p<=t)
+    {
+        solve(file,outfile,p);
+        ++p;
+    }
+    file.close();
+    outfile.close();
+    return 0;
+}
